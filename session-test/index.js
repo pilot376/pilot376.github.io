7f53b3901eb182cce41477241523d4621c1d9dd5
yiyang <yiyang@cafe24corp.com>
2025-01-14 13:53:20 +0900

    src_path 확인

--- session-test/index.js
+++ session-test/index.js
@@ -1,3 +1,30 @@
 (async function(){
-  console.log('hello');
+  var sTime = new Date().getTime();
+  console.log(sTime);
+  var src_path = window.cfaObject;
+  console.log(src_path);
+      // var idx_server_end = src_path.indexOf("/cfa.js");
+      // var goServer = src_path.substring(0,idx_server_end);
+    
+      // var s_uid = window.cfaUid ;
+      // var s_shop_no = window.cfaSno ;   //shop_no
+      // var service_type = window.cfaStype ;  //서비스구분 (e / g / w)
+      // var s_etc = window.cfaEtc ;     //기타변수지정
+    
+      // var cfa_CUK45='' , cfa_CID='' , cfa_CUK1Y='' , utype , cfa_domain , cfa_LC='' , new_visit ='T' , oLc1='' ,oLc2='' , doc_stm_log='' , fi_str ;
+      // var today = new Date();
+      
+      // const cookie_day = 45 ;
+      // const cookie_1_year = 365 ;
+      
+      // cfa_domain = window.cfaDomain ;
+      // if( !cfa_domain ) {
+      //   var cmc_temp_domain = document.URL.match(/https?\:\/\/(www\.)?([^\/]*)/);
+      //   if(cmc_temp_domain&&cmc_temp_domain[2]) { 
+      //     cfa_domain = "."+cmc_temp_domain[2];
+      //   }
+      // }
+      // else {
+      //   cfa_domain = "."+cfa_domain ;
+      // }
 })();
