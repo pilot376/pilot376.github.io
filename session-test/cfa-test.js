f4c1fe8c1a167f00786deffba4bc45fe23a9b908
yiyang <yiyang@cafe24corp.com>
2025-01-15 14:26:31 +0900

    method로 변경

--- session-test/cfa-test.js
+++ session-test/cfa-test.js
@@ -1,6 +1,6 @@
 (function() {
   window.addEventListener("message", (event) => {
-    if (event.data?.type === "setPermanentId") {
+    if (event.data?.method === "setPermanentId") {
       console.log(
         "Received client session ID:",
         event.data.params.id
