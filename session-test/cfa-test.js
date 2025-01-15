2f265b52dd580df94bce5158b2f430508f383395
yiyang <yiyang@cafe24corp.com>
2025-01-15 13:16:22 +0900

    포스트 메시지를 js로 뺌

---
+++ session-test/cfa-test.js
@@ -0,0 +1,30 @@
+(function() {
+  const iframe = document.getElementById("iframe");
+
+  iframe.onload = () => {
+    iframe.contentWindow.postMessage(
+      {
+        type: "REQUEST_SESSION",
+      },
+      "*"
+    );
+  };
+  
+  window.addEventListener("message", (event) => {
+    if (event.data?.type === "SESSION_INFO") {
+      console.log(
+        "Received client session ID:",
+        event.data.data.client_session_id
+      );
+    }
+  });
+})();
+
+
+
+
+
+
+
+
+
