964c27096ae107bcc2860aee8625b45778321536
yiyang <yiyang@cafe24corp.com>
2025-01-15 14:24:27 +0900

    객체 구조 변경

--- session-test/cfa-test.js
+++ session-test/cfa-test.js
@@ -1,20 +1,9 @@
 (function() {
-  const iframe = document.getElementById("iframeCFATestSession");
-
-  iframe.onload = () => {
-    iframe.contentWindow.postMessage(
-      {
-        type: "REQUEST_SESSION",
-      },
-      "*"
-    );
-  };
-  
   window.addEventListener("message", (event) => {
-    if (event.data?.type === "SESSION_INFO") {
+    if (event.data?.type === "setPermanentId") {
       console.log(
         "Received client session ID:",
-        event.data.data.client_session_id
+        event.data.params.id
       );
     }
   });
