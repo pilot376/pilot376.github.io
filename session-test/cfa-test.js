e3d594fb982bae7ecf3aeceda9905399dbec230a
yiyang <yiyang@cafe24corp.com>
2025-01-15 13:54:39 +0900

    allowedOrigin 추가

--- session-test/cfa-test.js
+++ session-test/cfa-test.js
@@ -11,6 +11,14 @@
   };
   
   window.addEventListener("message", (event) => {
+    const allowedOrigin = 'https://pilot376.github.io';
+    // 메시지를 보낸 출처(origin) 검증
+    if (event.origin !== allowedOrigin) {
+        console.warn('허용되지 않은 출처에서의 메시지:', event.origin);
+        return;
+    }
+
+	console.log(event.data);
     if (event.data?.type === "SESSION_INFO") {
       console.log(
         "Received client session ID:",
